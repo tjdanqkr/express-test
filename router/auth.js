bf7cee594b5e1fcf185f3aa286a75bad5cf5ae2f
tjdanqkr <tjdanqkr@gmail.com>
2024-09-11 05:50:42 +0900

    fix auth

--- router/auth.js
+++ router/auth.js
@@ -7,7 +7,7 @@ router.post("/register", (req, res) => {
   const { name, password } = req.body;
   if (!name || !password) return res.status(400).send("이름과 비밀번호를 입력해주세요.");
 
-  if (!names.includes(name)) return res.status(400).send("없는 이름입니다.");
+  // if (!names.includes(name)) return res.status(400).send("없는 이름입니다.");
 
   if (users.find((user) => user.name === name)) return res.status(400).send("이미 존재하는 이름입니다.");
   const id = users.length === 0 ? users.length + 1 : users[users.length - 1].id + 1;
